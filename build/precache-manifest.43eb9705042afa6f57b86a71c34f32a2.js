030526f9bd82c19062af6724c85dfbdced73db7d
Surbhi Airan <surbhiairan@gmail.com>
2019-09-05 13:29:30 +0530

    build

---
+++ build/precache-manifest.43eb9705042afa6f57b86a71c34f32a2.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed46d31064fc1cc110d4191923d1de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647c7f7695b47c249e17",
+    "url": "/static/css/2.a18897dd.chunk.css"
+  },
+  {
+    "revision": "1f20939f15ccd19493a6",
+    "url": "/static/css/main.531e17f8.chunk.css"
+  },
+  {
+    "revision": "647c7f7695b47c249e17",
+    "url": "/static/js/2.826cf5cf.chunk.js"
+  },
+  {
+    "revision": "1f20939f15ccd19493a6",
+    "url": "/static/js/main.3b79ea6d.chunk.js"
+  },
+  {
+    "revision": "fd95ba8e3feb07376be5",
+    "url": "/static/js/runtime~main.8685e9af.js"
+  },
+  {
+    "revision": "c89875fea4e9077e3e5631d6566af5dc",
+    "url": "/static/media/bannerlogo.c89875fe.png"
+  },
+  {
+    "revision": "511a51d557ad910c773bb91ec2ee9ae3",
+    "url": "/static/media/bg1.511a51d5.svg"
+  },
+  {
+    "revision": "63b9898bbaecd1d666cb1d8f8884f589",
+    "url": "/static/media/bg2.63b9898b.svg"
+  },
+  {
+    "revision": "a0a61e01b337202438944b249f4f3816",
+    "url": "/static/media/bg3.a0a61e01.svg"
+  },
+  {
+    "revision": "db53ad05f06919db89fb5e308a658cf8",
+    "url": "/static/media/dataimg1.db53ad05.png"
+  },
+  {
+    "revision": "1e410db1e591c43222f3caffee8f4952",
+    "url": "/static/media/dataimg2.1e410db1.png"
+  },
+  {
+    "revision": "f848c3ddbc416b3f48cfa8f165e6faaf",
+    "url": "/static/media/dataimg3.f848c3dd.png"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "8636b4252324d26fbe46ca3cba9deff2",
+    "url": "/static/media/insights_img1.8636b425.jpg"
+  },
+  {
+    "revision": "6c6a5493f4ce627dbdcc595b8f3fe279",
+    "url": "/static/media/insights_img2.6c6a5493.jpg"
+  },
+  {
+    "revision": "a6f4fc6ff5fc2c031a199e4f74ed8634",
+    "url": "/static/media/insights_img3.a6f4fc6f.jpg"
+  },
+  {
+    "revision": "20a542e6c005dd0feab2d5420acf1b92",
+    "url": "/static/media/meet.20a542e6.svg"
+  },
+  {
+    "revision": "f703eca5da63a7a95223baae0fa56122",
+    "url": "/static/media/reallyfast.f703eca5.svg"
+  },
+  {
+    "revision": "a288386be400d61888599c554c333a26",
+    "url": "/static/media/refreshinglysimple.a288386b.svg"
+  },
+  {
+    "revision": "dd8720b142b2f8abf687b02de89d69f4",
+    "url": "/static/media/remarkablyaffordable.dd8720b1.svg"
+  },
+  {
+    "revision": "b3648a04a59d3f198041baa120ad5ee4",
+    "url": "/static/media/technology.b3648a04.svg"
+  },
+  {
+    "revision": "1401221384b701161e8174c3487f3dba",
+    "url": "/static/media/vision.14012213.svg"
+  },
+  {
+    "revision": "3af4b0908a250179898d1a5150a22c93",
+    "url": "/static/media/whoweare.3af4b090.svg"
+  }
+]);
